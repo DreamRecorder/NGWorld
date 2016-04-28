309239495ac14627840dfa7bad3f4bcd080ed906
DLaboratory <dlaboratory@outlook.com>
2016-04-28 17:35:33 +0800

    建立目录结构并实现BKDR哈希算法作为示例

---
+++ shared/hash.cpp
@@ -0,0 +1,37 @@
+/*
+ *  This file is part of NEWorld-NG.
+ *
+ *  NEWorld-NG is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NEWorld-NG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NEWorld-NG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hash.h"
+using namespace std;
+namespace NEWorld
+{
+
+    namespace shared
+    {
+
+        unsigned int bkdr_hash(const string &str)
+        {
+            unsigned int h = 0;
+            for(int i = 0; i < str.size(); i++)
+                h = h * 131 + str[i];
+            return (h & 0x7FFFFFFF);
+        }
+
+    }
+
+}
+
---
+++ shared/hash.h
@@ -0,0 +1,35 @@
+/*
+ *  This file is part of NEWorld-NG.
+ *
+ *  NEWorld-NG is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NEWorld-NG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NEWorld-NG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _HASH_H_
+#define _HASH_H_
+
+#include <string>
+
+namespace NEWorld
+{
+
+    namespace shared
+    {
+
+        unsigned int bkdr_hash(const std::string &str);
+
+    }
+
+}
+
+#endif
