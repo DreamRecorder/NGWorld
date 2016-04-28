7fa39c91ba39f94e8acbda0e8e3f66ed53eb046f
DLaboratory <dlaboratory@outlook.com>
2016-04-28 21:44:32 +0800

    Rename the project to 'NGWorld'

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# NEWorld-NG
-下一代NEWorld，少一份糟粕，多一份洁净。
+# NGWorld
+实在是遭不住[NEWorld](https://github.com/Infinideastudio/NEWorld)的糟糕代码结构，决定从零开始编写一个用C++复刻Minecraft的游戏。少一份糟粕，多一份洁净。
 
-鉴于NEWorld的糟糕结构，在其上做修改所耗费的精力太大，还不如从零开始重写整个结构。
+这是一个**实验研究性**项目，作者可能会在数据结构、算法、常数优化、图形渲染上面来来回回地花费很多工夫，做一些benchmark实验，写一写论文什么的。这**不是**实用性项目。
+
+本项目的源代码采用[GPLv3](http://www.gnu.org/licenses/gpl.html)许可协议发布，请遵守和尊重。
--- shared/hash.h
+++ shared/hash.h
@@ -1,18 +1,18 @@
 /*
- *  This file is part of NEWorld-NG.
+ *  This file is part of NGWorld.
  *
- *  NEWorld-NG is free software: you can redistribute it and/or modify
+ *  NGWorld is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  NEWorld-NG is distributed in the hope that it will be useful,
+ *  NGWorld is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with NEWorld-NG.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with NGWorld.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _HASH_H_
