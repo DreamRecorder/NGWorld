1fdf209aef8c1bcda94d9e23b011b208b058a731
DLaboratory <dlaboratory@outlook.com>
2016-05-03 22:37:27 +0800

    Create NGWorld::OSLayer

---
+++ shared/fundamental_utility.cpp
@@ -0,0 +1,57 @@
+/*
+ * This file is part of NGWorld.
+ * (C) Copyright 2016 DLaboratory
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "fundamental_utility.h"
+
+#ifdef NGWORLD_OS_UNIX
+// include UNIX头文件
+#include <unistd.h>
+#elif NGWORLD_OS_WINDOWS
+// include Windows头文件
+#error NGWorld support for Windows platform is not implemented yet.
+#endif
+
+using namespace NGWorld;
+
+void OSLayer::sleep_us(const int &us)
+{
+#ifdef NGWORLD_OS_UNIX
+    ::usleep(us);
+#elif NGWORLD_OS_WINDOWS
+#error NGWorld support for Windows platform is not implemented yet.
+#endif
+}
+
+void OSLayer::sleep_ms(const int &ms)
+{
+#ifdef NGWORLD_OS_UNIX
+    ::usleep(ms * 1000);
+#elif NGWORLD_OS_WINDOWS
+#error NGWorld support for Windows platform is not implemented yet.
+#endif
+}
+
+void OSLayer::sleep_s(const int &s)
+{
+#ifdef NGWORLD_OS_UNIX
+    ::sleep(s);
+#elif NGWORLD_OS_WINDOWS
+#error NGWorld support for Windows platform is not implemented yet.
+#endif
+}
+
---
+++ shared/fundamental_utility.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of NGWorld.
+ * (C) Copyright 2016 DLaboratory
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _FUNDAMENTAL_UTILITY_H_
+#define _FUNDAMENTAL_UTILITY_H_
+
+#include "fundamental_macros.h"
+
+namespace NGWorld
+{
+    namespace OSLayer
+    {
+        void sleep_us(const int &us);
+        void sleep_ms(const int &ms);
+        void sleep_s(const int &s);
+    }
+}
+
+#endif
