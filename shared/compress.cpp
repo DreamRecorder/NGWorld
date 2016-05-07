a2e4c10a70deb9fd1c5636a2eedfe8ab7e66530c
DLaboratory <dlaboratory@outlook.com>
2016-05-07 14:59:00 +0800

    将QuickLZ隔离入namespace并编写压缩wrapper
    在Core i5-5250U上用编译参数
    clang++ -O2 -march=native -mtune=native
    可以得到409MB/s左右的压缩速度

---
+++ shared/compress.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of NGWorld.
+ * (C) Copyright 2016 DLaboratory
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "compress.h"
+#include "quicklz.h"
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+size_t compress(const char *src, char *dest, size_t size)
+{
+    QuickLZ::qlz_state_compress *state = new QuickLZ::qlz_state_compress;
+    size_t compressed_size = QuickLZ::qlz_compress(src, dest, size, state);
+    delete state;
+    return compressed_size;
+}
+
+size_t decompress(const char *src, char *dest)
+{
+    QuickLZ::qlz_state_decompress *state = new QuickLZ::qlz_state_decompress;
+    size_t decompressed_size = QuickLZ::qlz_decompress(src, dest, state);
+    delete state;
+    return decompressed_size;
+}
--- shared/fundamental_structure.h
+++ shared/fundamental_structure.h
@@ -23,8 +23,8 @@ typedef char s8;
 typedef unsigned char u8;
 typedef short s16;
 typedef unsigned short u16;
-typedef long s32;
-typedef unsigned long u32;
+typedef int s32;
+typedef unsigned int u32;
 typedef long long s64;
 typedef unsigned long long u64;
 
